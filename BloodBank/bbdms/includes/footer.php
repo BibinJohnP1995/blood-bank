04a22572177491eaa8a37140bcecfea85a5f7e53
Bibin John P <54799690+BibinJohnP1995@users.noreply.github.com>
2020-10-27 19:52:30 +0530

    Add files via upload

---
+++ BloodBank/bbdms/includes/footer.php
@@ -0,0 +1,8 @@
+  
+  
+  <footer class="py-5 bg-inverse">
+        <div class="container">
+           <marquee> <p class="m-0 text-center text-white" style = "font-family:georgia,garamond,serif;font-size:32px;">“There is no great joy than saving a soul.”</p></marquee>
+        </div>
+        <!-- /.container -->
+    </footer>
